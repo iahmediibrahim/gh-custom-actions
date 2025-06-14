3692d2013861bfb27395a2b75ae389255c33b4f2
iahmediibrahim <iahmediibrahim@gmail.com>
2025-06-14 21:12:06 +0300

    add custom js action

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,8 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const exec = require('@actions/exec')
+function run(params) {
+	core.notice('Deploying to AWS S3')
+}
+
+run()
